dc345adf37ac9693622bc464c736d61df641c9c0
manuarg1231~ <manu571@gmail.com>
2022-02-03 15:55:25 -0300

    Se agrego el mock de tareas y  su inteface

---
+++ src/app/Task.ts
@@ -0,0 +1,6 @@
+export interface Task{
+    id?: number;
+    text:string;
+    dia:string;
+    reminder:boolean;
+}
---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,36 @@
+import { Task } from "./Task"
+
+export const TASK: Task[]=[
+    {
+        id:1,
+        text:'Terminar proyecto de angular',
+        dia:'diciembre 22 a las 12:00',
+        reminder:true
+    },
+    {
+        id:2,
+        text:'Pasear al perro',
+        dia:'diciembre 22 a las 15:00',
+        reminder:false
+    },
+    {
+        id:3,
+        text:'Limpiar la casa',
+        dia:'diciembre 22 a las 17:00',
+        reminder:true
+    },
+    {
+        id:4,
+        text:'Merendar',
+        dia:'diciembre 22 a las 18:00',
+        reminder:false
+    },
+    {
+        id:5,
+        text:'Rendir examen',
+        dia:'diciembre 23 a las 08:00',
+        reminder:true
+    }
+]
+
+
